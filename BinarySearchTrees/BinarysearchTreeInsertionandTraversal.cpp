bd2f721dbd6bf97ff44a1f906d22b05f528899c4
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-03-09 21:14:46 +0530

    binary Search Tree basics added

---
+++ BinarySearchTrees/BinarysearchTreeInsertionandTraversal.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+       this ->data=d;
+       this->right=NULL;
+       this->left=NULL;
+    }
+};
+
+
+
+Node * insertIntoBST(Node* root,int data){
+    if (root==NULL)
+    {
+        root=new Node(data);
+        return root;
+    }
+
+    if(data>root->data){
+       root->right=insertIntoBST(root->right,data);
+    }else
+    {
+        root->left=insertIntoBST(root->left,data);
+    }
+    return root;    
+}
+
+void levelOrderTraversal(Node *& root){
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node* temp=q.front();
+        q.pop();
+
+        if (temp==NULL)
+        {
+            cout<<endl;
+        
+        if (!q.empty())      
+        {
+            q.push(NULL);
+        }
+    }else
+    {
+        cout<<temp->data<<" ";
+        
+        if (temp->left)
+        {
+            q.push(temp->left);
+        }
+        if (temp->right)
+        {
+            q.push(temp->right);
+        }
+        
+        
+    }
+    
+}
+}
+
+void getInput(Node*&root){
+    int data;
+    cout<<"Enter the data"<<endl;
+    cin>>data;
+
+    while (data!=-1)
+    {
+       root= insertIntoBST(root,data);
+        cin>>data;
+    }
+    
+}
+
+int main()
+{
+    Node * root=NULL;
+    getInput(root);
+    levelOrderTraversal(root);
+    return 0;
+}
